dc9ba315c2cab5e3da05955f142c1011cfa3fcec
Shady Alfred <shadyalfred98@gmail.com>
2023-11-16 23:24:03 +0200

    `update` ssh-agent

--- configuration.nix
+++ configuration.nix
@@ -307,6 +307,7 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+  programs.ssh.startAgent = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
